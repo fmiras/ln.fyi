fa9fd128647556f086fb86a29170ebb0ff8ec805
Federico Miras <miras.federico@gmail.com>
2024-12-01 01:55:59 -0300

    fix: convert node page to client component

--- app/node/[publicKey]/page.tsx
+++ app/node/[publicKey]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import { Progress } from '@/components/ui/progress'
