f837fae676b4c90e0355dcdc28f8129c96e58e60
Federico Miras <miras.federico@gmail.com>
2024-12-12 21:05:26 -0300

    feat: add invoice page metadata

--- app/[invoice]/page.tsx
+++ app/[invoice]/page.tsx
@@ -1,7 +1,32 @@
+import { Metadata } from 'next'
+
 import LightningInvoice from '@/components/lightning-invoice'
 import { decode } from '@/lib/decode'
 
-export default async function InvoicePage({ params }: { params: Promise<{ invoice: string }> }) {
+interface PageProps {
+  params: Promise<{
+    invoice: string
+  }>
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const invoiceHash = (await params).invoice
+  const invoice = decode(invoiceHash)
+
+  return {
+    title: `${invoice.description} - Lightning Invoice Details - ln.fyi`,
+    description: `View detailed statistics and information about Lightning Network invoice ${
+      invoice.description
+    }. Amount: ₿${(invoice.amount / 100_000_000).toLocaleString()}`,
+    openGraph: {
+      title: `${invoice.description} - Lightning Invoice Details`,
+      description: `View detailed statistics and information about Lightning Network invoice ${invoice.description}`,
+      type: 'website'
+    }
+  }
+}
+
+export default async function InvoicePage({ params }: PageProps) {
   const invoiceHash = (await params).invoice
   const invoice = decode(invoiceHash)
 
